7f36405cf7d9a73187a62e42d6870ae661ad0602
Rediet Yemane <redietamare04@gmail.com>
2024-05-31 18:05:11 +0300

    backend integrated

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: "http://127.0.0.1:5000",
-  baseURL: "http://127.0.0.1:5001/clone-a8c42/us-central1/api",
+  // baseURL: "http://127.0.0.1:5001/clone-a8c42/us-central1/api",
+  baseURL :"https://amazon-api-deploy-x8m9.onrender.com/"
 });
 
 export { axiosInstance };
